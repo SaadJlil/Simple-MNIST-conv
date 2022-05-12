7462a06fa8071504746b80d7dc4c24d43604793c
whoknowsreally <81829990+whoknowsreally@users.noreply.github.com>
2022-05-12 23:46:34 +0200

    Add files via upload

---
+++ mnist.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR 
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 28, 3, 1)
+        self.conv2 = nn.Conv2d(28, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+def train(model, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        print(loss)
+        optimizer.step()
+
+epoch = 10000
+lr = 1e-2
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+
+#add your local directory here
+
+data = datasets.MNIST("DIRECTORY FILE", train = True, download = True, transform = transform) 
+
+train_loader = torch.utils.data.DataLoader(data, batch_size = 20, shuffle = True)
+
+model = Net() 
+
+optimizer = optim.SGD(model.parameters(), lr)
+
+train(model, train_loader, optimizer, epoch) 
+
+
